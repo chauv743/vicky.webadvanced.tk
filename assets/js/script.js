04a82d6e6b7226c936a0873252ea0a3bb2dfa21d
chauv743 <Chauv743@newschool.edu>
2016-10-21 16:44:23 +0000

    update with domain project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,49 @@
 $("h1").click(function () {
-      $(this).hide();
+      // $(this).hide();
       $("#wrapper").show();
 });
 
 //HELLO WORLD
 $("#hello-world").click(function () {
+      $("h1").hide();
       $("#hello-world-info").slideDown();
 });
 
 $("#domain, #pattern-generator, #sender-receiver").click(function () {
+      $("h1").hide();
       $("#hello-world-info").hide();
 });
 
 //PATTERN GENERATOR
 $("#pattern-generator").click(function () {
+      $("h1").hide();
       $("#pattern-generator-info").slideDown();
 });
 
 $("#hello-world, #domain, #sender-receiver").click(function () {
+      $("h1").hide();
       $("#pattern-generator-info").hide();
 });
 
 //DOMAIN
 $("#domain").click(function () {
+      $("h1").hide();
       $("#domain-info").slideDown();
 });
 
 $("#hello-world, #pattern-generator, #sender-receiver").click(function () {
+      $("h1").hide();
       $("#domain-info").hide();
 });
 
 
 //SENDER RECEIVER
 $("#sender-receiver").click(function () {
+      $("h1").hide();
       $("#sender-receiver-info, #smiley").slideDown();
 });
 
 $("#hello-world, #pattern-generator, #domain").click(function () {
+      $("h1").hide();
       $("#sender-receiver-info").hide();
 });
